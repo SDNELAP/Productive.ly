d82fa7a772b1f369da700ca336d92fbd82a825ff
Sthitadheesh Nelapatla <dheeshn@gmail.com>
2017-08-28 17:15:32 -0400

    Updated task list system

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 /**
  * Created by Nelapatla on 8/27/17.
  */
-var tasks = [];
 
 function addNewTask(newTaskText){
-    console.log("Entered Function");
-    tasks.push(newTaskText);
-
     var task_list = document.getElementById('task_list');
     var new_element = document.createElement("li");
 
